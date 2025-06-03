ef55578cffa7b30861f0ae35baac5e3c5cc799d1
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-06-03 16:23:02 +0200

    added frontend routing

---
+++ frontend/src/types.d.ts
@@ -0,0 +1,20 @@
+import authClient from './services/auth-client';
+
+declare global {
+  type Session = typeof authClient.$Infer.Session;
+
+  type OutletData = {
+    data: Session | null;
+    isPending: boolean;
+  };
+
+  type ProtectedOutletData = OutletData & {
+    protectedData: null | object;
+  };
+
+  type NavbarProps = {
+    user: Session['user'] | undefined;
+    handleSignIn: () => Promise<void>;
+    handleSignOut: () => Promise<void>;
+  };
+}
